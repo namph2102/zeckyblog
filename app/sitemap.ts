bbd79b14bb54b036e1f180638023930c80c2c71b
Nam <namph2102@gmail.com>
2023-07-17 14:31:02 +0700

    fixed sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,15 @@
 import { IData } from "./sevices/typedata";
 import { getData } from "./sevices/untils";
-
+interface Fulldata extends IData {
+  createdAt: string;
+  updatedAt: string;
+}
 const domain = process.env.DOMAIN_URL;
 export default async function sitemap() {
   const data = await getData();
 
-  return data.map((blog: IData) => ({
+  return data.map((blog: Fulldata) => ({
     url: `${domain}/blog/${blog.slug}`,
-    lastModified: new Date().toISOString(),
+    lastModified: blog.updatedAt,
   }));
 }
